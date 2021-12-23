0f828ed58cfb6f92b244334ca2e99d441bc18d94
tomato <tomatono@gmail.com>
2021-12-23 23:59:56 +0900

    yamlの読み込み

--- civ6-utils/src/main.ts
+++ civ6-utils/src/main.ts
@@ -1,1 +1,9 @@
-console.log('test');
+import fs from 'fs';
+import YAML from 'yaml';
+
+// 設定ファイルを読み込む
+const file = fs.readFileSync('./config/config.yaml', 'utf8')
+const config = YAML.parse(file);
+
+// TODO debug
+console.log('config=%o', config);
